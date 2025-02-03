dfde252befedb62d304c986c269c3a76f033d0e7
Mulf <63870279+mulfdev@users.noreply.github.com>
2025-02-03 15:24:26 -0600

    Update palette.lua

--- lua/no-clown-fiesta/palette.lua
+++ lua/no-clown-fiesta/palette.lua
@@ -1,10 +1,10 @@
 local colors = {
   none = "NONE",
-  fg = "#E1E1E1",
+  fg = "#A9A9A9",
   bg = "#151515",
   alt_bg = "#171717",
   accent = "#202020",
-  white = "#E1E1E1",
+  white = "#A9A9A9",
   gray = "#373737",
   medium_gray = "#727272",
   light_gray = "#AFAFAF",
